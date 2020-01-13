521ab2260f3bab7f4ff223a022f8f0dcbd26898a
Ali Asadi <aliesaassadi@gmail.com>
2020-01-13 17:20:16 +0200

    add sample-app

---
+++ sample-app/src/main/java/com/spotim/sample/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.spotim.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import spotIm.common.SpotCallback
+import spotIm.common.SpotException
+import spotIm.sdk.SpotIm
+
+/**
+ * Created by Ali Asadi on 13/01/2020
+ **/
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //TODO - Use your SpotId
+        SpotIm.init(this, "sp_xxx")
+
+        SpotIm.getPreConversationFragment("test", object : SpotCallback<Fragment> {
+            override fun onSuccess(fragment: Fragment) {
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+            }
+
+            override fun onFailure(exception: SpotException) {
+                Log.d("MainActivity", exception.toString())
+            }
+
+        })
+    }
+}
