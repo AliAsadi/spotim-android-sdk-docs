4bbe192629d97026d6895e02733ebf1e7d8c921d
Ali Asadi <aliesaassadi@gmail.com>
2020-02-20 11:58:21 +0200

    Use ConversationOptions to get customize results

--- sample-app/src/main/java/com/spotim/sample/MainActivity.kt
+++ sample-app/src/main/java/com/spotim/sample/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import spotIm.common.SpotCallback
 import spotIm.common.SpotException
+import spotIm.common.options.ConversationOptions
+import spotIm.common.sort.SortType
 import spotIm.sdk.SpotIm
 
 /**
@@ -20,7 +22,12 @@ class MainActivity : AppCompatActivity() {
         //TODO - Use your SpotId
         SpotIm.init(this, "sp_xxx")
 
-        SpotIm.getPreConversationFragment("test", object : SpotCallback<Fragment> {
+        val options = ConversationOptions.Builder()
+            .addMaxCountOfPreConversationComments(4)
+            .addSortType(SortType.SORT_BEST)
+            .build()
+
+        SpotIm.getPreConversationFragment("test", options, object : SpotCallback<Fragment> {
             override fun onSuccess(fragment: Fragment) {
                 supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
             }
